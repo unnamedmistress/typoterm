c16b3c925c778ddae47af25df29ef2896f1dd5e9
unknown <delgadojustin100@yahoo.com>
2023-03-10 14:18:45 -0500

    made fixs

--- src/component/coverletter.js
+++ src/component/coverletter.js
@@ -1,12 +1,10 @@
-// import "bootstrap/dist/css/bootstrap.min.css";
-
 import React, { useState, useRef, useEffect } from "react";
 import generateText from "../openai.js";
 const prompt = "Write coverletter ";
 const response = generateText;
 
 
-const coverletter = (props) => {
+const coverLetter = (props) => {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>Heading</h1>
