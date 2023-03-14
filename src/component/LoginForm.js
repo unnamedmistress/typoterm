604beaa44a9ba082e1245f24918f0d51a4b633fa
darioelao <darioelao@gmail.com>
2023-03-14 12:55:45 -0400

    footer section

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -103,6 +103,30 @@ function LoginForm(props) {
     </div>
   </div>
 </div>
+
+
+<footer class="bg-gray-200 py-8">
+  <div class="container mx-auto flex flex-wrap">
+    <div class="w-full lg:w-1/3 px-4">
+      <h3 class="font-bold text-gray-900 mb-4">About Us</h3>
+      <p class="text-gray-700">Small paragraph.</p>
+    </div>
+    <div class="w-full lg:w-1/3 px-4">
+      <h3 class="font-bold text-gray-900 mb-4">Social Media</h3>
+      <ul class="list-reset">
+        <li class="mb-2">
+          <a href="#" class="text-gray-700 hover:text-blue-500"><i class=""></i> github</a>
+        </li>
+      </ul>
+    </div>
+    <div class="w-full lg:w-1/3 px-4">
+      <h3 class="font-bold text-gray-900 mb-4">Contact Us</h3>
+      <p class="text-gray-700"><i class="fas fa-phone-square fa-lg"></i> 1-800-123-4567</p>
+      <p class="text-gray-700"><i class="fas fa-envelope fa-lg"></i> info@example.com</p>
+    </div>
+  </div>
+</footer>
+
 </>
 
   );
