8828a9c84a34bfa73d7e2cd99cfd4609a7f83733
unknown <delgadojustin100@yahoo.com>
2023-03-14 10:53:00 -0400

    put coverletter in nav

--- src/component/coverletter.js
+++ src/component/coverletter.js
@@ -3,44 +3,13 @@ import generateText from "../openai.js";
 const prompt = "Write coverletter ";
 const response = generateText;
 
-
-const coverLetter = (props) => {
+const Cover = (props) => {
   return (
     <div>
-      <h1 style={{ textAlign: "center" }}>Heading</h1>
-       <p style={{ textAlign: "center" }}>instructions </p>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="username"
-            value={username}
-            placeholder="user1"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            value={password}
-            placeholder="password1"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary mr-2">Log in</button>
-        <button type="button" className="btn btn-secondary" onClick={props.onSignupClick}>
-          Sign up
-        </button>
-
-      </form>
+      <h1 style={{ textAlign: "center" }}>Headlking</h1>
+      <p style={{ textAlign: "center" }}>instructions </p>
     </div>
   );
-
 }
 
-export default coverLetter;
+export default Cover; 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './component/tailwind.css';
-
 import Nav from "./component/Nav.js";
 import How from "./component/how.js";
 import Contact from "./component/contact.js";
 import Essay from "./component/essay.js";
 import Outline from "./component/outline.js";
+import Cover from "./component/coverletter.js"
 import UserAccount from "./component/useraccount.js";
 import ResponseForm from "./component/ResponseForm.js";
 import LoginForm from "./component/LoginForm.js";
 import SignupForm from "./component/SignupForm.js";
+
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -26,6 +26,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/contact" element={<Contact />} />
         <Route path="/essay-helper" element={<Essay />} />
         <Route path="/topic-outline-generator" element={<Outline />} />
+        <Route path="/coverLetter" element={<Cover />} />
         <Route path="/useraccount" element={<UserAccount />} />
         <Route path="/response-form" element={<ResponseForm />} />
       </Routes>
