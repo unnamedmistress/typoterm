9d89490f461a617d73bada05bbda4c3f8ce1e4f0
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-03-14 20:24:38 -0400

    outline changes

--- src/component/outline.js
+++ src/component/outline.js
@@ -33,33 +33,31 @@ function Outline() {
   const [outputText, setOutputText] = useState("");
 
   return (
-    <div className="mx-auto max-w-md p-6">
-      <h1 className="text-3xl font-bold mb-4 text-center">Heading</h1>
-      <p className="text-lg mb-6 text-center">Instructions</p>
+    <div className="py-10">
+      <h1 className="text-5xl font-bold text-center text-gray-900 mb-8">Topic Generator</h1>
+      <p className="text-lg font-semibold text-center text-gray-700 mb-8">Follow these instructions to create your Essay.</p>
+      <br></br><br></br>
       <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="inputText" className="block mb-2 font-semibold">
-            Input Text:
-          </label>
+        <div className="w-1/2 mx-auto p-4">
           <input
             type="text"
-            className="border p-2 w-full"
+            className="w-full h-32 bg-gray-100 border border-gray-400 rounded py-2 px-4"
             id="inputText"
             value={inputText}
-            placeholder="Enter text here"
+            placeholder="Write your Essay here.."
             onChange={(e) => setInputText(e.target.value)}
           />
         </div>
         <button
           type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
         >
           Submit
         </button>
       </form>
       {outputText && (
-        <div className="mt-6">
-          <h2 className="text-xl font-bold mb-2">Output Text:</h2>
+        <div className="mt-5">
+          <h2 className="text-xl font-bold mb-4">Output Text:</h2>
           <p className="text-lg">{outputText}</p>
         </div>
       )}
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,30 +1,30 @@
-import jwt_decode from 'jwt-decode';
-const secret = 'mysecrettypo';
-const expiration = '2h';
+// import jwt_decode from 'jwt-decode';
+// const secret = 'mysecrettypo';
+// const expiration = '2h';
 
-export default function authMiddleware(req, res, next) {
-  let token = req.body.token || req.query.token || req.headers.authorization;
+// export default function authMiddleware(req, res, next) {
+//   let token = req.body.token || req.query.token || req.headers.authorization;
 
-  if (req.headers.authorization) {
-    token = token.split(' ').pop().trim();
-  }
+//   if (req.headers.authorization) {
+//     token = token.split(' ').pop().trim();
+//   }
 
-  if (!token) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
+//   if (!token) {
+//     return res.status(401).json({ error: 'Unauthorized' });
+//   }
 
-  try {
-    const decodedToken = jwt_decode(token);
-    const { data } = decodedToken;
-    req.user = data;
-    next();
-  } catch (err) {
-    console.log('Invalid token');
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-}
+//   try {
+//     const decodedToken = jwt_decode(token);
+//     const { data } = decodedToken;
+//     req.user = data;
+//     next();
+//   } catch (err) {
+//     console.log('Invalid token');
+//     return res.status(401).json({ error: 'Unauthorized' });
+//   }
+// }
 
-export function signToken({ email, username, _id }) {
-  const payload = { email, username, _id };
-  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-}
+// export function signToken({ email, username, _id }) {
+//   const payload = { email, username, _id };
+//   return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+// }
