5147a3d6743524062686cc424d6317310d1ceb0e
UnnamedMistress <momchrysti@gmail.com>
2023-03-16 01:59:34 -0400

    getting log to render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-// Import necessary libraries
 import React, { useState, useEffect } from "react";
 import LoginForm from "./component/LoginForm.js";
 import SignupForm from "./component/SignupForm.js";
-import { SignupButton } from "./component/SignButton.js";
 import AuthButtons from "./component/LogButton.js";
 import LogoutButton from "./component/LogoutButton.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -13,19 +11,14 @@ import Essay from "./component/essay.js";
 import UserAccount from "./component/useraccount.js";
 import ResponseForm from "./component/ResponseForm.js";
 
-
-// Define the App component
 const App = () => {
-  // Define states for text input and loading state
   const [text, setText] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  // Define states for user login and sign-up
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showSignupForm, setShowSignupForm] = useState(false);
   const [isFormLoaded, setIsFormLoaded] = useState(false);
 
-  // Check if user is already logged in on component mount
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
@@ -33,7 +26,6 @@ const App = () => {
     }
   }, []);
 
-  // Functions to handle user login, logout and sign-up
   const handleLogin = () => {
     setIsLoggedIn(true);
   };
@@ -47,10 +39,8 @@ const App = () => {
     setShowSignupForm(true);
   };
 
-  // Define state for the loading dots animation
   const [dots, setDots] = useState(0);
 
-  // Use effect to create and clear interval for loading dots animation
   useEffect(() => {
     const intervalId = setInterval(() => {
       setDots((prevDots) => (prevDots + 1) % 4);
@@ -60,11 +50,9 @@ const App = () => {
 
   return (
     <div className="App">
-    {/* Render the LoginForm component */}
-    <LoginForm />
-  </div>
-);
+   
+    </div>
+  );
 };
-
 
 export default App;
