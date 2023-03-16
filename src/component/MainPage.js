1f33886afdb4acbf2f81ac41907136d581e5c3dc
UnnamedMistress <momchrysti@gmail.com>
2023-03-16 14:36:57 -0400

    homepage

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -29,17 +29,34 @@ const MainPage = () => {
                 Sign Up
               </Link>
               <div className="flex flex-col items-center justify-center h-screen">
-            <img
+            
+            </div><img
               src="https://github.com/unnamedmistress/typoterm/blob/main/public/assets/IMG_2662.PNG?raw=true"
               alt="Centered"
               className="max-w-full max-h-full h-64 mb-8"
             />
-            </div>
             <div className="py-12">
               <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
                 <h2 className="text-3xl font-extrabold text-white mb-4">Reviews</h2>
                 <div className="flex flex-wrap -mx-4 group">
-                  {/* ... */}
+                <div className="w-full md:w-1/3 px-4 mb-8">
+  <div className="bg-zinc-500 opacity-70 rounded-lg shadow-xl group-hover:blur-sm hover:!blur-none p-6">
+    <p className="text-lg font-medium mb-4 text-teal-200 hover:text-white">"I recently came across an app called Typo Terminators, which has been incredibly helpful in enhancing the quality of my cover letters. As cover letters can be challenging to write, I was looking for a user-friendly tool that could provide me with guidance"</p>
+    <p className="text-gray-400 hover:text-gray-300">- Justin BiBer</p>
+  </div>
+</div>
+<div className="w-full md:w-1/3 px-4 mb-8">
+  <div className="bg-zinc-500 opacity-70 rounded-lg shadow-xl group-hover:blur-sm hover:!blur-none p-6">
+    <p className="text-lg font-medium mb-4 text-teal-200 hover:text-white">"I tried out an app named "Typo Terminator" that helps me write an essay for my class. I was blown away by how dramatically it helped me. The app was super easy to use and it provided me with all the tools I needed to write a great essay that the final product is error-free"</p>
+    <p className="text-gray-400 hover:text-gray-300">- Ferris Bueller</p>
+  </div>
+</div>
+<div className="w-full md:w-1/3 px-4 mb-8">
+  <div className="bg-zinc-500 opacity-70 rounded-lg shadow-xl group-hover:blur-sm hover:!blur-none p-6">
+    <p className="text-lg font-medium mb-4 text-teal-200 hover:text-white">"What impressed me most about Typo Terminators is its ability to identify and correct any spelling or grammar errors, which is crucial when submitting a job application. The app's built-in proofreading tool ensures that the final product is error-free and professional-looking. "</p>
+    <p className="text-gray-400 hover:text-gray-300">-Mario</p>
+  </div>
+</div>
                 </div>
               </div>
             </div>
